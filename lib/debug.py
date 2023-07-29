61bfc1f2ba038514713b5c7fedc1e2be29d308ec
sandyjtech <sandyjtech@gmail.com>
2023-07-29 14:18:10 -0700

    added class attributes"

---
+++ lib/debug.py
@@ -0,0 +1,9 @@
+import ipdb
+
+from song import Song
+
+new_song = Song("new song", "sandy", "pop")
+new_song3 = Song("third song", "Jazz Man", "Jazz")
+
+
+ipdb.set_trace()
--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,47 @@
+
+
 class Song:
-    pass
+    count = 0
+    genres = []  
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre        
+        Song.add_song_to_count()
+        Song.add_to_genres(self)        
+        Song.add_to_artists(self)
+        Song.add_to_genre_count(self)
+        Song.add_to_artist_count(self)
+        
+    def add_song_to_count():
+        Song.count += 1        
+        
+    def add_to_genres(self):        
+        Song.genres.append(self.genre)
+        
+    def add_to_genre_count(self):
+        Song.genre_count.update({f"{self.genre}": 0})
+        for genre in Song.genres:            
+            if genre == self.genre:                        
+                Song.genre_count[self.genre] += 1
+                print(f"Adding Genres {genre} : {Song.genre_count[self.genre]}")               
+                                            
+    def add_to_artists(self):
+        Song.artists.append(self.artist)
+        
+    def add_to_artist_count(self):
+        Song.artist_count.update({f"{self.artist}": 0})
+        for artist in Song.artists:            
+            if artist == self.artist:                        
+                Song.artist_count[self.artist] += 1
+                print(f"Adding Genres {artist} : {Song.artist_count[self.artist]}") 
+           
+    #def artist_count(self):
+        
+                          
+            
+       
